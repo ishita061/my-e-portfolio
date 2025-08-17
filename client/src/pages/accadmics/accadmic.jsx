d18f907eccb16b51f2eb60474893e5633dff1b88
Ishita chaturvedi <168914437+ishita061@users.noreply.github.com>
2025-08-17 10:50:21 +0530

    Update accadmic.jsx

--- client/src/pages/accadmics/accadmic.jsx
+++ client/src/pages/accadmics/accadmic.jsx
@@ -10,7 +10,7 @@ const Accadmics = () => {
         <h2>BTech in Computer Science and Engineering</h2>
         <p><strong>Institution:</strong> Graphic Era Hill University, Dehradun</p>
         <p><strong>Duration:</strong> 2023 – 2027</p>
-        <p><strong>CGPA:</strong> 8.97</p>
+        <p><strong>CGPA:</strong> 7.4</p>
         <ul>
           <li>Core subjects: Data Structures, Algorithms, Operating Systems, DBMS, Networking</li>
           <li>Projects in MERN stack, Full Stack-Development and Machine Learning </li>
@@ -22,7 +22,7 @@ const Accadmics = () => {
         <h2>Class 12th</h2>
         <p><strong>Institution:</strong> ST. Thomas School, Loni</p>
         <p><strong>Year of Completion:</strong> 2023</p>
-        <p><strong>Percentage:</strong> 88.6%</p>
+        <p><strong>Percentage:</strong> 72%</p>
         <ul>
           <li>Core subjects: Physics, Chemistry, Mathematics, Computer Science</li>
         </ul>
@@ -32,7 +32,7 @@ const Accadmics = () => {
         <h2>Class 10th</h2>
         <p><strong>Institution:</strong> ST. Thomas School, Sahibabad</p>
         <p><strong>Year of Completion:</strong> 2021</p>
-        <p><strong>Percentage:</strong> 90.2%</p>
+        <p><strong>Percentage:</strong> 80%</p>
         <ul>
           <li>Core subjects: Mathematics, Science, English, Social Studies, Computer Science</li>
         </ul>
